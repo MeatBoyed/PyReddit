71469d9fc71456566b34e047a28aced53b02afc0
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-08-10 14:24:00 +0200

    feat: User Registration functional

--- PyReddit/users/views.py
+++ PyReddit/users/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from .CustomForms import UserRegistrationForm
 from django.contrib import messages
+
 
 # Create your views here.
 def registerPage(request):
 
 	if request.method == "POST":
-		form = UserCreationForm(request.POST)	
+		form = UserRegistrationForm(request.POST)	
 		if form.is_valid():
+			form.save()
 			username = form.cleaned_data.get("username")
 			messages.success(request, f"Account created for {username}. Please Login")
 			return redirect("user-login")
 	else:
-		form = UserCreationForm()
+		form = UserRegistrationForm()
 
 	context = {
 		"title": "Create Account",
