f14eef6fe617105f2b081058381444bfc129a965
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-08-15 12:20:14 +0200

    feat: About field added to Profile

--- PyReddit/PyReddit/urls.py
+++ PyReddit/PyReddit/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib.auth import views as authViews
 from users.views import registerPage, profilePage 
@@ -13,3 +15,7 @@ urlpatterns = [
     path("logout/", authViews.LogoutView.as_view(template_name="users/logoutPage.html"), name="user-logout"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
